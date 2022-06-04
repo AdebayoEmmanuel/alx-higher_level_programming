507f0e5dce5f4dcaa08b7e445e4aff15b3a20829
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-04 01:50:01 +0000

    if __name__== "__main__": import...

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a:d} + {b:d} = {result:d}".format(a, b, result))
+    from add_0 import add
+
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a:d} + {b:d} = {result:d}".format(a, b, result))
