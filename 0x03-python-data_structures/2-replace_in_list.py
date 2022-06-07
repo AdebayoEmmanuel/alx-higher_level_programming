4d71ee46982595f7d3db3be93c8b2ae829235e8c
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-07 03:54:05 +0000

    FIX pycodestyle 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,10 +5,10 @@ def replace_in_list(my_list, idx, element):
     Args:
         my_list: list object containing integers
         idx: the index of the element to be replaced
-        element: element to be inserted 
+        element: element to be inserted
 
-    Returns: Original list if idx is negative or out of range, otherwise returns
-             modified list with element at index idx replaced
+    Returns: Original list if idx is negative or out of range, otherwise
+            returns modified list with element at index idx replaced
 
     """
     if idx >= 0 and idx < len(my_list):
