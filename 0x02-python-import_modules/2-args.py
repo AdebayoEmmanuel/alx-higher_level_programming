bb850417d346e4963f998d37c06365c22db07828
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-07 21:05:55 +0000

    FIX .formats in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(count))
     elif arg_count == 2:
         count == 1
-        print("{:d} argument:".formats(count))
+        print("{:d} argument:".format(count))
     else:
         for i in range(1, len(args), 1):
             count += 1
