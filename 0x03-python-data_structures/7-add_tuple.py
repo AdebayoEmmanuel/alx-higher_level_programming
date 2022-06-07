840d5d716465a65a08f7769ec5d6210ab7490ea4
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-07 05:51:40 +0000

    CREATE 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,37 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """This function adds two tuples
 
         Args:
-            tuple_a: first tuple to be added
-            tuple_b: second tuple to be added
+            tuple_a: The first tuple argument
+            tuple_b: The second tuple argument
 
-        Return: a tuple of two ints =, derived by adding tuple_a and tuple:b
+        Return: a tuple with two integers
     """
-    a_len = len(tuple_a)
-    b_len = len(tuple_b)
 
-# if tuples have less than 2 elements, use 0 to replace missing element
-    if a_len < 2:
-        if a_len == 1:
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    # This block checks if the tuple have at least 2 elements
+    if len_a < 2:
+        if len_a == 1:
             tuple_a = (tuple_a[0], 0)
         else:
             tuple_a = (0, 0)
-    if b_len < 2:
-        if b_len == 1:
+    elif len_b < 2:
+        if len_b == 1:
             tuple_b = (tuple_b[0], 0)
         else:
             tuple_b = (0, 0)
-    res = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-    return res
+
+    # This block groups the respective elements as tuples
+    tup_elem = ()
+    for a, b in zip(tuple_a, tuple_b):
+        tup_elem += ((a, b),)
+
+    # sums the respective first and second elements
+    sum_first = tup_elem[0][0] + tup_elem[0][1]
+    sum_sec = tup_elem[1][0] + tup_elem[1][1]
+    return_tuple = (sum_first, sum_sec)
+
+    return (return_tuple)
