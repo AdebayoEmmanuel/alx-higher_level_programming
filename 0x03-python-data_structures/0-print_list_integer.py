408a0e819b6a7481a6e18e673aa8cf34bc74d3a6
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-07 03:24:19 +0000

    CREATE 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """A function that prints integers
+
+    Args:
+        my_list: list of integers to be printed
+
+    Returns: None
+
+    """
     for i in range(len(my_list)):
         print(f"{my_list[i]:d}".format(my_list[i]))
