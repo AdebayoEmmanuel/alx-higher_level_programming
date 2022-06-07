3904af588e8f38bcd951ce04dcf8f2fb23afcbae
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-07 04:06:13 +0000

    implementedt reverse print list with for loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """This funciton prints the element of list in reverse
+
         Args:
             my_list: the list to be printed
+
         Return: None
     """
     last_idx = len(my_list) - 1
