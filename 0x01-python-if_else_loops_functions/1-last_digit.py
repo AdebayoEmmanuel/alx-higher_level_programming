f6498ed25fa86f1787c3afd52fa42dbed4cd2595
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-11 11:01:26 +0000

    ADD pycodestyle support for last digit play

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-# turn number to string 
+# turn number to string
 number = str(number)
 # get the last digit of number and store in a var
 lastDigit = number[-1]
@@ -15,10 +15,12 @@ if number[0] == "-":
 number = int(number)
 # check if last digit is 0, print the message
 if lastDigit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number,lastDigit))
+    print("Last digit of {:d} is {:d} and is 0".format(number, lastDigit))
 # check if greater than 5, print the message
 if lastDigit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number,lastDigit))
+    print("Last digit of {:d} is {:d} and is greater than 5".format(
+        number, lastDigit))
 # check if less than 6, print the message
 if lastDigit < 6 and lastDigit != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,lastDigit))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(
+        number, lastDigit))
