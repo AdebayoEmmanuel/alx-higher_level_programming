8c90ce44713dd70c902f30bffd0a0318ef14bb98
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-11 14:33:33 +0000

    to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     """A function that converts a string to uppercase, similar to string.upper()
-        
+
        Args:
        str: string to be converted
 
@@ -10,7 +10,7 @@ def uppercase(str):
     """
     for i in range(len(str)):
         upperStr = str[i]
-        if ord(str[i]) in range(97,123):
+        if ord(str[i]) in range(97, 123):
             upperStr = chr((ord(str[i]))-32)
         print("{}".format(upperStr), end="")
-    print()        
+    print()
