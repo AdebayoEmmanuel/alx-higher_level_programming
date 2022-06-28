0d916e315a126be0b0fcd7194a5984912f90b7c0
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-06-28 03:55:38 +0000

    PROPS getters plus setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,38 +3,42 @@ class Rectangle:
     """This class defines a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Instantiotion method for rectangle class"""
-        self.__height = height
-        self.__width = width
+        """Instantiate an objet with width and height"""
+
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """retrieves width property of a rectangle"""
+        """Retrieve width"""
 
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets the width of rectangle to value"""
+        """set object width to value"""
+
         if type(value) is not int:
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
     @property
     def height(self):
-        """retrieves the height property of a rectangle"""
+        """retrieve height"""
 
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height of a rectangle to value"""
+        """Set object height to value"""
+
         if type(value) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
+
         if value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
