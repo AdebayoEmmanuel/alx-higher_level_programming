c57a100cf3bdb8f09a75ee1619c68db888726bd2
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-11 16:56:12 +0000

    ADD area public method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,7 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
+
+    def area(self):
+        """returns area of a rectangle"""
+        return self.height * self.width
