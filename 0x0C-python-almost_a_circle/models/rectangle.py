a14bc6f99b7d0e783bc25f787d3a9164c5f4dcb6
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-11 07:03:58 +0000

    first sub class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,3 +8,21 @@ from models.base import Base
 class Rectangle(Base):
     """This class has even weirder init"""
 
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """okay not that weird, just using Base id"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        self.__width = value
