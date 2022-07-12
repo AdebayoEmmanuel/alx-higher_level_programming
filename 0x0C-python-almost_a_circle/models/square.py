8fc0cc56b5abb2a3dd3d1096059446ce1e31f5d2
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-12 14:22:23 +0000

    ADD update method with variable arga and kwargs for square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,14 +3,18 @@
     sqaure class that inherits
     the rectangle class
 """
+
+
 from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """A square class that extends our rectangle class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """initialize by inheriting rectangle's behaviours"""
         super().__init__(size, size, x, y, id)
-    
+
     @property
     def size(self):
         """Getter for square size"""
@@ -29,3 +33,28 @@ class Square(Rectangle):
                                                  self.y,
                                                  self.size)
 
+    def update(self, *args, **kwargs):
+        """this method updates square with
+            variable positional and keyword
+            arguments
+        """
+
+        for i in len(args):
+            if i == 0:
+                self.id = args[i]
+            elif i == 1:
+                self.size = args[i]
+            elif i == 2:
+                self.x = args[i]
+            elif i == 3:
+                self.y = args[i]
+
+        if not args or if len(args) == 0:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
