85ccb43248edd6cbdc5fda4df62545f3642ce919
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-12 16:31:21 +0000

    ADD all main modules

--- 0x0C-python-almost_a_circle/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""I crested this module to print
-    my variadic arguments in update()
-"""
-from models.rectangle import Rectangle
-
-recto = Rectangle(89, 2, 3, 4, 5)
-recto.update(1, 2, 3, 4, 6)
