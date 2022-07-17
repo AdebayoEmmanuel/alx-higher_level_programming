0dcf65134a3eb963f8b00c2e132ebee965641e76
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-17 00:43:09 +0000

    CREATE base geometry class that raises error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This base geometry raises error"""
+
+
+class BaseGeometry:
+    """A class that raises an error"""
+    def area:
+        """Raises an exception"""
+        raise("area() is not implemented")
