258a2575815ef7c1ded49527357e32c762d85a30
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-08-01 04:23:36 +0000

    DOCUMENT function

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this function returns python object from json."""
+import json
+
+
+def append_write(my_str):
+    """Decodes normal Python object from JSON.
+    Args:
+        my_str(str): JSON string representation
+    Returns:
+        Python object
+    """
+    return JSON.loads(my_str)
