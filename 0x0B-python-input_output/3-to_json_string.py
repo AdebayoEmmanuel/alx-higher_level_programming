a0ff5031ee01b076d26433efe6dcf94739c3e37b
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-08-01 04:19:52 +0000

    DOCUMENT function

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this function returns JSON."""
+import json
+
+
+def append_write(my_obj):
+    """Encodes normal Python object string to JSON.
+    Args:
+        my_obj(str): a regular python object 
+    Returns:
+        JSON representation od my_obj
+    """
+    return JSON.dumps(my_obj)
