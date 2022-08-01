8d9f8c07754044f50195cc4069e0893b4d5716ce
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-08-01 05:41:24 +0000

    FIX obj naming in 8-to_json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,6 +2,6 @@
 """This module generates the dictionary description of an obj"""
 
 
-def class_to_json(ob):
+def class_to_json(obj):
     """Returns a dictionary representation of a class and all its attributes"""
     return obj.__dict__
