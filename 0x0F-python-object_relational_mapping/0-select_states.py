8ff4c01a49c13b32e499b7bb8c9429f22985e96a
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-09-11 03:38:07 +0000

    CHANGE keyword space for root

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@ import MySQLdb
 import sys
 
 db = MySQLdb.connect(host="localhost", port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                     user=sys.argv[2], passwd=sys.argv[1], db=sys.argv[3])
 
 cur = db.cursor()
 
