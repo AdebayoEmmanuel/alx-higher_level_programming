c58aad228615c20be467e3a7ad7fc2536f67c0e3
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-09-11 17:48:45 +0000

    ADD pycodestyle compliance

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -9,12 +9,16 @@ from sqlalchemy.orm import sessio
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}\
+            @localhost/{dbname}', pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=eng)
     session = Session()
-    
+
     for state in session.query(state).order_by(state.id):
         print('{}: {}'.format(state.id, state.name))
     session.close()
