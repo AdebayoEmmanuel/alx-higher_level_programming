a0d3890cd1af38256edcf142e0562a2e60f13bc6
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-09-11 06:18:25 +0000

    GUARD state search from SQL injection attacks

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""filter states starting with N, ordered by state id"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    # execute SQL query
+    cur.execute(
+        "SELECT id, name FROM states WHERE name LIKE %s\
+                ORDER BY states.id ASC;", (sys.argv[4],))
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
