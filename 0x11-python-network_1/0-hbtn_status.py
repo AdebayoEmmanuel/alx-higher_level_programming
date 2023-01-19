650bc650b857955db1a1d10c6877dbe74ce5b91a
Emmanuel <adebayodemilade7@gmail.com>
2023-01-19 08:57:27 +0000

    REMOVE function def

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-
-
-import urllib.request
-
 '''This script fetches alx status endpoint'''
 
 
-def hbtn_stat():
-    '''fetches an endpoint and print some info about it'''
+if __name__ == '__main__':
+    import urllib.request
+
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         output = response.read()
 
-    print("Body response:")
-    print("\t - type:", type(output))
-    print("\t - content:", output)
-    print("\t - utf8 content:", output.decode("utf-8"))
-
-
-if __name__ == "__main__":
-    hbtn_stat()
+        print("Body response:")
+        print("\t - type:", type(output))
+        print("\t - content:", output)
+        print("\t - utf8 content:", output.decode("utf-8"))
