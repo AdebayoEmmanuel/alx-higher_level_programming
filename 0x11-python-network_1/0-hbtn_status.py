73f88b42284a72154bca22ffe5820fdc30380d4c
Emmanuel <adebayodemilade7@gmail.com>
2023-01-19 08:51:28 +0000

    ADD pycodestyle validation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+
+
 import urllib.request
 
 '''This script fetches alx status endpoint'''
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    output = response.read()
 
-print("Body response:")
-print("\t - type:", type(output))
-print("\t - content:", output)
-print("\t - utf8 content:", output.decode("utf-8"))
+
+def hbtn_stat():
+    '''fetches an endpoint and print some info about it'''
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        output = response.read()
+
+    print("Body response:")
+    print("\t - type:", type(output))
+    print("\t - content:", output)
+    print("\t - utf8 content:", output.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    hbtn_stat()
