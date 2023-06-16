834586942d45745abd577e895375b545cace5c57
Emmanuel (Demilade) Adebayo <92952014+AdebayoEmmanuel@users.noreply.github.com>
2023-06-16 09:28:57 +0000

    semistandard validation

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-let mappedVal = list.map((x) => x * list.indexOf(x));
+const mappedVal = list.map((x) => x * list.indexOf(x));
 console.log(JSON.stringify(list));
 console.log(JSON.stringify(mappedVal));
