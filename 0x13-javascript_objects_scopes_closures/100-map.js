bc9e885272cdf500f8622d7f61b252bad3a053d4
Emmanuel (Demilade) Adebayo <92952014+AdebayoEmmanuel@users.noreply.github.com>
2023-06-16 09:24:53 +0000

    owned map

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const list = require('./100-data').list;
+let mappedVal = list.map((x) => x * list.indexOf(x));
+console.log(JSON.stringify(list));
+console.log(JSON.stringify(mappedVal));
