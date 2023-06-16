7b0150a65f137876edd27c955f987a25d84caa9e
Emmanuel (Demilade) Adebayo <92952014+AdebayoEmmanuel@users.noreply.github.com>
2023-06-16 09:29:34 +0000

    remove json.stringify

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 const mappedVal = list.map((x) => x * list.indexOf(x));
-console.log(JSON.stringify(list));
-console.log(JSON.stringify(mappedVal));
+console.log(list);
+console.log(mappedVal);
